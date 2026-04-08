411759e7737b06d30b23a0f06c7e52bc7c43c3d6
Akash Bhagat <126611007+beakash@users.noreply.github.com>
2026-04-08 17:17:23 +0530

    Implement loop examples in Loops.java
    Added examples of for, while, and do-while loops in Java.

---
+++ Loops.java
@@ -0,0 +1,30 @@
+//for loops
+Public class Loops {
+  Public static void main(String args[]){
+    for(int counter = 0; counter< 3;counter++){
+      System.out.println("Hello world");
+    }
+  }
+}
+
+//print 0 1 2 3 4 5 6 7 8 9 10
+
+for(int i = 0; i < 11 ; i++){
+  print(i);
+}
+//While Loops ( same kaam ko krne ka 2nd way )
+
+int i = 0;
+while(i<11){
+  System.out.println(i);
+  i = i + 1;
+}
+//do while loop  
+
+int i = 0;
+do {
+  System.out.println(i);
+  i = i + 1;
+}
+  while (i<11);
+}
